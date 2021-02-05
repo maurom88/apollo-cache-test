a82457b1496e5afb4b349cc24c3bbb772b0e5dba
Mauro <mauro.mden@gmail.com>
2021-02-04 21:08:28 -0600

    Create most simple local-only fields + client-side schema example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import './App.css';
+import React from 'react';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  useQuery,
+  gql
+} from '@apollo/client';
 
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+const typeDefs = gql`
+  extend type Query {
+    name: String
+  }
+`;
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache()
-});
-
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        name: {
+          read() {
+            return 'American Dollar';
+          }
         }
       }
-    `
-  })
-  .then((result) => console.log(result));
+    }
+  }
+});
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io/',
+  cache,
+  typeDefs
+});
+
+const EXCHANGE_RATES = gql`
+  query GetRates {
+    # rates(currency: "USD") {
+    #   currency
+    # }
+    name @client
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>{data.name}</div>
+  )
+}
 
 function App() {
-  return <div></div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo App</h2>
+        <ExchangeRates />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
