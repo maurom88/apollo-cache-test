9cb541d5c44aafb4ef4010e19bb60b461977de17
Mauro <mauro.mden@gmail.com>
2021-02-17 06:16:47 -0600

    Move AllCurrencies to separate file

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import './App.css';
 import React from 'react';
-import { ApolloProvider, ApolloClient, useQuery, gql } from '@apollo/client';
+import { ApolloProvider, ApolloClient } from '@apollo/client';
 
-import { cache, currencyCodesVar } from './cache';
+import { cache } from './cache';
 
 import Currency from './components/Currency';
+import AllCurrencies from './components/AllCurrencies';
 
 // Create new instance of Apollo client
 const client = new ApolloClient({
   // Import cache defined above
   cache
 });
-
-// Query the allCurrencies field on the client
-const ALL_CURRENCIES_QUERY = gql`
-  query GetAllCurrencies {
-    allCurrencies @client
-  }
-`;
-
-// Component that renders a list of all currencies
-function AllCurrencies() {
-  const { loading, error, data } = useQuery(ALL_CURRENCIES_QUERY);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message} </p>;
-
-  // Add all currency codes into reactive variable currencyCodesVar
-  currencyCodesVar(
-    data.allCurrencies.map((currency) => {
-      return currency.code;
-    })
-  );
-
-  const allCurrencies = data.allCurrencies;
-
-  return (
-    <div>
-      {allCurrencies.map((currency) =>
-        currency.visible ? (
-          <p key={currency.code}>
-            {currency.name} {currency.code} {currency.symbol}{' '}
-          </p>
-        ) : null
-      )}
-    </div>
-  );
-}
 
 function App() {
   return (
