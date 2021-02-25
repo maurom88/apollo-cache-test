9fdc4212f7cd1ed42b899a85c60ebb1eb25346da
Mauro <mauro.mden@gmail.com>
2021-02-24 20:50:59 -0600

    Create form to add a currency

--- src/components/AddCurrency.js
+++ src/components/AddCurrency.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+function handleSubmit(e) {
+  e.preventDefault()
+}
+
 export default function AddCurrency() {
   return (
     <div>
-      <p></p>
+     <form onSubmit={handleSubmit}>
+       <label htmlFor="currency-name">Currency name: </label>
+       <input type="text" name="currency-name"></input>
+       <br />
+       <label htmlFor="currency-name">Currency code: </label>
+       <input type="text" name="currency-code"></input>
+       <br />
+       <label htmlFor="currency-name">Currency symbol: </label>
+       <input type="text" name="currency-symbol"></input>
+       <br />
+       <button type="submit">Submit</button>
+     </form>
     </div>
   );
 }
